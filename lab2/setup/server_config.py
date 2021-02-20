c29a8d8f65a0101ba9a580d134578e96a1d88ac5
zandent <simon.zhao@mail.utoronto.ca>
2021-02-20 03:03:39 -0500

    init part2

---
+++ lab2/setup/server_config.py
@@ -0,0 +1,6 @@
+# Do not alter this file
+
+hostname        = 'id.ca1.bioconnectid.com'
+username        = 'ece568'
+password        = 'XdjXSaPba.3JjCe4'
+
---
+++ lab2/setup/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ./get-pip.py --user --no-warn-script-location
+~/.local/bin/pip3 install --user requests qrcode-terminal --no-warn-script-location
+
