2c36641493ff72c68839eb0612c0054655b3a7ab
zandent <simon.zhao@mail.utoronto.ca>
2021-03-20 18:04:14 -0400

    update part2

--- lab4/dnsproxy_starter.py
+++ lab4/dnsproxy_starter.py
@@ -2,7 +2,7 @@
 import argparse
 import socket
 from scapy.all import *
-
+import sys
 # This is going to Proxy in front of the Bind Server
 
 parser = argparse.ArgumentParser()
@@ -17,4 +17,24 @@ port = args.port
 dns_port = args.dns_port
 # Flag to indicate if the proxy should spoof responses
 SPOOF = args.spoof_response
+
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('127.0.0.1', port)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+while True:
+    print >>sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(4096)
+    
+    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    print (data)
+    
+    if data:
+        sent = sock.sendto(data, address)
+        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+
 
